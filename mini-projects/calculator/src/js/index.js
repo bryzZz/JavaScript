7e26313b6318cf30ac404c33868edff68d44a8b8
Nikita <tebloev.nikita@gmail.com>
2021-07-23 23:43:25 +0900

    added unary minus and showing expression on top

--- mini-projects/calculator/src/js/index.js
+++ mini-projects/calculator/src/js/index.js
@@ -1,67 +1,103 @@
 'use strict';
 
+import Typed from './Typed';
 import '../scss/style.scss';
 
 const calculator = document.querySelector('.calculator'),
-      showLine = calculator.querySelector('.calculator__show-line');
+      showLine = calculator.querySelector('.calculator__answer-line'),
+      answerLine = calculator.querySelector('.calculator__show-line');
 
 let expression = '';
 
+//possible operations
+const operators = {
+    '+': {
+        fn: (first, second) => first + second,
+        priority: 1
+    },
+    '-': {
+        fn: (first, second) => first - second,
+        priority: 1
+    },
+    '*': {
+        fn: (first, second) => first * second,
+        priority: 2
+    },
+    '/': {
+        fn: (first, second) => first / second,
+        priority: 2
+    },
+    '%': {
+        fn: (first, second) => first % second,
+        priority: 2
+    },
+    '^': {
+        fn: (first, second) => first ** second,
+        priority: 3
+    },
+}
+
 calculator.addEventListener('click', (e) => {
     if(!e.target.classList.contains('calculator__btn')) return;
-    if(e.target.dataset.option === "calculate"){
-        calculate(expression);
+
+    if(e.target.dataset.option === 'calculate'){
+        if(isExpressionCorrect(expression)){
+            const result = calculate(expression);
+
+            new Typed(answerLine, showLine.textContent + '=', 260);
+
+            showLine.textContent = result;
+            expression = result;
+        }
         return;
     }
-    if(e.target.dataset.option === "clear"){
+
+    if(e.target.dataset.option === 'clear'){
         expression = '';
         showLine.textContent = '0';
         return;
     }
 
-    if(showLine.textContent.trim() === '0'){
+    if(showLine.textContent === '0'){
+        if(e.target.dataset.option === 'option' && e.target.dataset.value !== '-'){
+            return;
+        }
+
         expression = e.target.dataset.value;
         showLine.textContent = e.target.textContent;
+    }else if(e.target.dataset.option === 'option' && showLine.textContent[showLine.textContent.length-1] in operators){
+        if(expression.length > 1){
+            expression = expression.slice(0, expression.length - 1) + e.target.dataset.value;
+            showLine.textContent = showLine.textContent.slice(0, showLine.textContent.length - 1) + e.target.textContent
+        }
     }else{
         expression += e.target.dataset.value;
         showLine.textContent += e.target.textContent;
     }
 });
 
+function isExpressionCorrect(strExpression){
+    strExpression = strExpression.trim();
+
+    if(strExpression === '0' || strExpression === '') return false;
+
+    return true;
+}
+
 function calculate(strExpression){
     // strExpression = '1+(2-3)*4*5+6*7';
     // strExpression = '2.2 ^ 3 + 2 * 2 ^ 3';
 
-    //possible operations
-    const operators = {
-        '+': {
-            fn: (first, second) => first + second,
-            priority: 1
-        },
-        '-': {
-            fn: (first, second) => first - second,
-            priority: 1
-        },
-        '*': {
-            fn: (first, second) => first * second,
-            priority: 2
-        },
-        '/': {
-            fn: (first, second) => first / second,
-            priority: 2
-        },
-        '%': {
-            fn: (first, second) => first % second,
-            priority: 2
-        },
-        '^': {
-            fn: (first, second) => first ** second,
-            priority: 3
-        },
-    }
+    // evaluating an expression by building a parse tree
+    let result = firstMethod(strExpression);
 
+    return result;
+}
+
+function firstMethod(strExpression){
     //tokenization
     const lexemes = tokenize(strExpression);
+    console.log(lexemes);
     // build syntax tree
     const syntaxTree = buldSyntaxTree(lexemes);
     // calculate result
@@ -73,16 +109,14 @@ function calculate(strExpression){
         let i = 0;
         while(i < strExpression.length){
             let s = strExpression[i];
-            if(s in operators){
+            if(s === '-' && !strExpression?.[i-1]){
+                lexemes.push(`${0 - +strExpression[i+1]}`);
+                i++;
+            }else if(s in operators){
                 lexemes.push(s);
             }else if(s === '('){
-                let blockInBrackets = [];
-                i++;
-                while(strExpression[i] !== ')'){
-                    blockInBrackets.push(strExpression[i]);
-                    i++;
-                }
-                lexemes.push(blockInBrackets);
+                lexemes.push(tokenize(strExpression.slice(i+1, strExpression.indexOf(')'))));
+                i = strExpression.indexOf(')') + 1;
             }else if(!isNaN(s)){
                 let str = s;
                 i++;
@@ -168,8 +202,5 @@ function calculate(strExpression){
         return syntaxTreeNode.value;
     }
 
-    console.log('expression:', strExpression);
-    console.log('lexemes:', lexemes);
-    console.log('syntaxTree:', syntaxTree);
-    console.log('result:', calculatedResult);
+    return calculatedResult;
 }
