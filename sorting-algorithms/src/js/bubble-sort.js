64153a87b6b84ec12ed2c2871e9e176d0989f591
Nikita <tebloev.nikita@gmail.com>
2021-08-02 14:47:00 +0900

    added simple sorting visualization

--- sorting-algorithms/src/js/bubble-sort.js
+++ sorting-algorithms/src/js/bubble-sort.js
@@ -1,16 +1,29 @@
-const unsortedArray = [1, 3, 6, 1, 3, 7, 1, 2, 10, 9, 22, 14];
+// const unsortedArray = [1, 3, 6, 1, 3, 7, 1, 2, 10, 9, 22, 14];
 
-function bubbleSort(arr){
+// function bubbleSort(arr){
+//     const arrLength = arr.length;
+//     for(let i = 0; i < arrLength; i++){
+//         for(let j = arrLength-1; j > i; j--){
+//             if (arr[j-1] > arr[j]) {
+//                 [arr[j-1], arr[j]] = [arr[j], arr[j-1]];
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(unsortedArray);
+// console.log(bubbleSort(unsortedArray));
+
+export default function* bubbleSort(arr){
     const arrLength = arr.length;
     for(let i = 0; i < arrLength; i++){
         for(let j = arrLength-1; j > i; j--){
             if (arr[j-1] > arr[j]) {
                 [arr[j-1], arr[j]] = [arr[j], arr[j-1]];
+                yield [[...arr], j-1, j];
             }
         }
     }
-    return arr;
+    // return arr;
 }
-
-console.log(unsortedArray);
-console.log(bubbleSort(unsortedArray));
--- sorting-algorithms/src/js/index.js
+++ sorting-algorithms/src/js/index.js
@@ -2,3 +2,95 @@
 
 import "../style.css";
 
+import { randomInteger } from './utils';
+
+import bubbleGen from './bubble-sort';
+
+import Chart from 'chart.js/auto';
+
+const sidebar = document.querySelector('.sidebar'),
+      container = document.querySelector('.placeToShow'),
+      ctx = document.querySelector('#myChart').getContext('2d');
+
+sidebar.addEventListener('click', (e) => {
+    if(!e.target.classList.contains('sidebar__item') || e.target.classList.contains('sidebar__item--active')) return;
+
+    sidebar.querySelector('.sidebar__item--active').classList.remove('sidebar__item--active');
+    e.target.classList.add('sidebar__item--active');
+
+    const sortingType = e.target.dataset.sortingType;
+});
+
+const arrayToSort = [];
+for(let i = 0; i < 10; i++){
+    arrayToSort.push(randomInteger(0, 100));
+
+    container.textContent += arrayToSort[i] + ' ';
+}
+
+console.log(arrayToSort);
+
+let gen = bubbleGen(arrayToSort);
+
+let interval = setInterval(showChanges, 3000);
+
+function showChanges(){
+    let nextValue = gen.next();
+    if(nextValue.done){
+        clearInterval(interval);
+    }else{
+        console.log(nextValue.value);
+        let arr = nextValue.value[0],
+            firstIndex = nextValue.value[1],
+            secondIndex = nextValue.value[2];
+
+        let newArr = document.createElement('div');
+
+        for(let i = 0; i < arr.length; i++){
+            let span = document.createElement('span');
+            span.textContent = arr[i] + ' ';
+            if(i === firstIndex || i === secondIndex){
+                span.style.color = 'lime';
+            }
+            newArr.append(span);
+        }
+
+        container.innerHTML = '';
+        container.append(newArr);
+    }
+}
+
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Bubble sort',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
